c9528dde6d709b0e9158dfe7c44b1d9ff68dff3f
DESKTOP-MNFIK6G\Cesar <cesar.encabo.gomez@alumnojoyfe.iepgroup.es>
2024-01-07 15:31:11 +0100

    [UPDATE] Commit intentando mostrar pantalla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './Pagina_Principal/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'main', component: MainComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './Pagina_Principal/cabecera/cabecera.component';
+import { MainComponent } from './Pagina_Principal/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
