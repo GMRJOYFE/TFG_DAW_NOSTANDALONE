3448e6f1f56dcf8a477d5aeab9edf876588cbeb3
DESKTOP-MNFIK6G\Cesar <cesar.encabo.gomez@alumnojoyfe.iepgroup.es>
2024-01-08 13:48:25 +0100

    [UPDATE] Módulos Pagina inicio y Página InicioSesión

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './Pagina_Principal/main/main.component';
+import { Main2Component } from './Pagina_InicioSesion/main2/main2.component';
 
 const routes: Routes = [
   {path: 'main', component: MainComponent},
+  {path: 'main2', component: Main2Component},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { CabeceraComponent } from './Pagina_Principal/cabecera/cabecera.component';
 import { MainComponent } from './Pagina_Principal/main/main.component';
 import { FooterComponent } from './Pagina_Principal/footer/footer.component';
+import { Body2Component } from './Pagina_Principal/body2/body2.component';
+import { PublicidadComponent } from './Pagina_Principal/publicidad/publicidad.component';
+import { PreciosComponent } from './Pagina_Principal/precios/precios.component';
+import { Main2Component } from './Pagina_InicioSesion/main2/main2.component';
+import { FormularioComponent } from './Pagina_InicioSesion/formulario/formulario.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +18,11 @@ import { FooterComponent } from './Pagina_Principal/footer/footer.component';
     CabeceraComponent,
     MainComponent,
     FooterComponent,
+    Body2Component,
+    PublicidadComponent,
+    PreciosComponent,
+    Main2Component,
+    FormularioComponent,
   ],
   imports: [
     BrowserModule,
