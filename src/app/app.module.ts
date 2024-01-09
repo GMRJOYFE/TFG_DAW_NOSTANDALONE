cce611ac0320f415bf27d3082e3f881b4aabe199
DESKTOP-MNFIK6G\Cesar <cesar.encabo.gomez@alumnojoyfe.iepgroup.es>
2024-01-09 22:15:09 +0100

    [UPDATE] Pagina Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { CabeceraComponent } from './Pagina_Principal/cabecera/cabecera.component';
 import { MainComponent } from './Pagina_Principal/main/main.component';
 import { FooterComponent } from './Pagina_Principal/footer/footer.component';
+import { Body1Component } from './Pagina_Principal/body1/body1.component';
 import { Body2Component } from './Pagina_Principal/body2/body2.component';
+import { Body3Component } from './Pagina_Principal/body3/body3.component';
 import { PublicidadComponent } from './Pagina_Principal/publicidad/publicidad.component';
 import { PreciosComponent } from './Pagina_Principal/precios/precios.component';
 import { Main2Component } from './Pagina_InicioSesion/main2/main2.component';
@@ -19,10 +21,12 @@ import { FormularioComponent } from './Pagina_InicioSesion/formulario/formulario
     MainComponent,
     FooterComponent,
     Body2Component,
+    Body1Component,
     PublicidadComponent,
     PreciosComponent,
     Main2Component,
     FormularioComponent,
+    Body3Component,
   ],
   imports: [
     BrowserModule,
